92f7d97d83914d9ba313eb0b650930adbcbea16c
chenyshi <chenyshi@yonyou.com>
2022-08-12 13:44:25 +0800

    update

---
+++ BlazorApp6/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp6.Pages
+{
+    public partial class Index
+    {
+        public string MyProperty { get; set; }
+        public void Click(string type)
+        {
+            MyProperty = $"{type} clicked";
+        }
+    }
+}
