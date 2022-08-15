ca45d82dcc9ed3827cbadf8d83569672b5a76f2f
chenyshi <chenyshi@yonyou.com>
2022-08-15 14:26:42 +0800

    update

---
+++ BlazorApp6/Pages/RazorDemo.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BlazorApp6.Pages
+{
+    [Authorize]
+    public partial class RazorDemo
+    {
+        public static object locker = new object();
+        public string UserName { get; set; } = "Jack";
+
+        public List<string> Hobbies { get; set; } = new List<string>() { "篮球", "睡觉" };
+
+        public async Task<string> GetUserName()
+        {
+            return await Task.FromResult(UserName);
+        }
+
+        public string GetUserName<T>(T name)
+        {
+            return name.ToString();
+        }
+    }
+}
